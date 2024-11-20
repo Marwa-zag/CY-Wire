6c4b19ec7cd748f6fa57c32c1c78ef02fda41472
Marwa-zag <131346380+Marwa-zag@users.noreply.github.com>
2024-11-20 10:38:23 +0100

    Add files via upload

--- Projet/c-wire.sh
+++ Projet/c-wire.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #Nom des arguments
-fichier_csv="$1"
-traitement="$2"
+fichier_dat="$1"
+type_station=$2
+type_consommateur=$3
 
 #Nom des dossiers
 temp="./temp"
@@ -22,4 +23,34 @@ fi
 if [ ! -d "$graphs" ]; then
     echo "Le dossier de graphiques n'existe pas, création en cours..."
     mkdir -p "$graphs"
+fi
+
+# Vérification de l'existence du fichier CSV
+if [ ! -f "$fichier_dat" ]; then
+    echo "Erreur : Le fichier CSV spécifié n'existe pas ($fichier_dat)."
+    exit 1
+fi
+# Vérification du type de station
+if [[ "$type_station" != "hvb" && "$type_station" != "hva" && "$type_station" != "lv" ]]; then
+    echo "Erreur : Type de station invalide ($type_station)."
+    echo "Les types valides sont : hvb, hva, lv."
+    exit 1
+fi
+
+# Vérification du type de consommateur
+if [[ "$type_consommateur" != "comp" && "$type_consommateur" != "indiv" && "$type_consommateur" != "all" ]]; then
+    echo "Erreur : Type de consommateur invalide ($type_consommateur)."
+    echo "Les types valides sont : comp, indiv, all."
+    exit 1
+fi
+
+# Vérification des combinaisons interdites
+if [[ "$type_station" == "hvb" && ( "$type_consommateur" == "all" || "$type_consommateur" == "indiv" ) ]]; then
+    echo "Erreur : La combinaison hvb avec all ou indiv est interdite."
+    exit 1
+fi
+
+if [[ "$type_station" == "hva" && ( "$type_consommateur" == "all" || "$type_consommateur" == "indiv" ) ]]; then
+    echo "Erreur : La combinaison hva avec all ou indiv est interdite."
+    exit 1
 fi
