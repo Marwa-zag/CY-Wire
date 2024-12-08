8f38a9b1d6704c0a534572ce44c4f4b755016270
Emmadr04 <dananmaaa@gmail.com>
2024-12-08 11:10:55 +0100

    création de la structure pour les stations

---
+++ Projet/avl.h
@@ -0,0 +1,16 @@
+#ifndef AVL_H
+#define AVL_H
+
+// structure permettant de contenir les informations d'une station
+
+typedef struct Station {
+    int station_id;              
+    double capacite; // Capacité de la station en MW 
+    double somme_consommateur; // Somme des valeurs de consommation 
+    struct Station* fg; // Pointeur vers le sous-arbre gauche
+    struct Station* fd; // Pointeur vers le sous-arbre droit
+    int hauteur; // Hauteur du nœud pour l'équilibre de l'AVL
+} Station;
+
+#endif
+
